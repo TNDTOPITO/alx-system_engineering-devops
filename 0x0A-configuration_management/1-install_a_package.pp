c8a94de33a350e5adc13e9f37b668fbce43dfd66
Thabiso <940303tabiso@gmail.com>
2022-12-09 08:10:21 -0800

    Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Installs puppet-lint, version 2.1.0
+# install puppet-lint -v 2.1.0
 
-package { 'puppet-lint':
-  ensure   => '2.1.0',
-  provider => 'gem',
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
 }
