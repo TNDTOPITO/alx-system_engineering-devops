a9f965a5ef3818c193acc6a61c770341bd2d3c87
Thabiso <940303tabiso@gmail.com>
2022-12-12 21:35:29 -0800

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Installs puppet-lint, version 2.1.1
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+# Installs flask, version 2.1.0
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
