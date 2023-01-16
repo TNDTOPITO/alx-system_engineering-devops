713fdf338d77555dcb5762304caeeb6f26185e11
Thabiso <940303tabiso@gmail.com>
2023-01-16 08:16:28 -0800

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python3
 """ Script that uses JSONPlaceholder API to get information about employee """
-import requests
-import sys
+import json
+import urllib.request
 
 
- url = f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/todos'
-    req = requests.get(url)
-    com_tasks = []
-    no = 0
-    for task in req.json():
-        if task.get('completed'):
-            com_tasks.append(task.get('title'))
-        no += 1
-    url = f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}'
-    req = requests.get(url)
-    name = req.json().get('name')
-    msg = f'Employee {name} is done with tasks({len(com_tasks)}/{no}:'
-    print(msg)
-    for task in com_tasks:
-        print('\t ' + task)
+def get_todo_list(employee_id):
+    url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    response = urllib.request.urlopen(url)
+    data = json.loads(response.read())
+
+    employee_name = ""
+    completed_tasks = []
+    total_tasks = len(data)
+    for task in data:
+        if task["completed"] == True:
+            completed_tasks.append(task["title"])
+        if employee_name == "":
+            user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+            user_response = urllib.request.urlopen(user_url)
+            user_data = json.loads(user_response.read())
+            employee_name = user_data["name"]
+    
+    print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):")
+    for task in completed_tasks:
+        print(f"\t {task}")
+
+employee_id = input("Enter employee ID: ")
+get_todo_list(int(employee_id))
+
