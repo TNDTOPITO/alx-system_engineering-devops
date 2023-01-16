b86af0a204f8a698fb61ab1b48cdab43c08fe0d6
Thabiso <940303tabiso@gmail.com>
2023-01-16 08:33:45 -0800

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,34 +1,32 @@
 #!/usr/bin/python3
 """ Script that uses JSONPlaceholder API to get information about employee """
+import json
+import urllib.request
 import csv
-import requests
-import sys
 
+def get_todo_list(employee_id):
+    url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    response = urllib.request.urlopen(url)
+    data = json.loads(response.read())
 
-if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
+    employee_name = ""
+    for task in data:
+        if employee_name == "":
+            user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+            user_response = urllib.request.urlopen(user_url)
+            user_data = json.loads(user_response.read())
+            employee_name = user_data["name"]
+    
+    filename = f"{employee_id}.csv"
+    with open(filename, mode='w', newline='') as csv_file:
+        fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        
+        for task in data:
+            writer.writerow({'USER_ID': employee_id, 'USERNAME': employee_name,
+                             'TASK_COMPLETED_STATUS': task["completed"], 'TASK_TITLE': task["title"]})
+    print(f"Tasks data exported to {filename}.")
 
-    userid = sys.argv[1]
-    user = '{}users/{}'.format(url, userid)
-    res = requests.get(user)
-    json_o = res.json()
-    name = json_o.get('username')
-
-    todos = '{}todos?userId={}'.format(url, userid)
-    res = requests.get(todos)
-    tasks = res.json()
-    l_task = []
-    for task in tasks:
-        l_task.append([userid,
-                       name,
-                       task.get('completed'),
-                       task.get('title')])
-
-    filename = '{}.csv'.format(userid)
-    with open(filename, mode='w') as employee_file:
-        employee_writer = csv.writer(employee_file,
-                                     delimiter=',',
-                                     quotechar='"',
-                                     quoting=csv.QUOTE_ALL)
-        for task in l_task:
-            employee_writer.writerow(task)
+employee_id = input("Enter employee ID: ")
+get_todo_list(int(employee_id))
