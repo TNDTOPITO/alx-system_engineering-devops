75f611fd444e7914e6ea615984c03afc045c197b
Thabiso <940303tabiso@gmail.com>
2023-02-05 19:10:17 -0800

    Adding Task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# Fixes bad `phpp` extensions to `php`
+# Fix 500 error when a GET HTTP method is requested to Apache web server
 
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+exec {'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
